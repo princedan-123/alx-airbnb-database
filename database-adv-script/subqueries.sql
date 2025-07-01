cc226c36e37304904b5e88f0a881ee1a33e9e9ec
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-01 15:14:56 +0100

    used new table name

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,7 +1,7 @@
 -- Objective: Write both correlated and non-correlated subqueries.
 -- Write a query to find all properties
 -- where the average rating is greater than 4.0 using a subquery.
-SELECT property_id, name FROM Property WHERE property_id IN(SELECT property_id FROM Review  GROUP BY property_id HAVING AVG(rating));
+SELECT property_id, name FROM property WHERE property_id IN(SELECT property_id FROM review  GROUP BY property_id HAVING AVG(rating));
 
 -- Write a correlated subquery to find users who have made more than 3 bookings.
-SELECT user_id FROM User u WHERE (SELECT COUNT(*) FROM Booking b WHERE u.user_id = b.user_id) > 3;
+SELECT user_id FROM users u WHERE (SELECT COUNT(*) FROM booking b WHERE u.user_id = b.user_id) > 3;
