e6463a742785ab9dbe68295672137ba114117d1b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-01 14:26:59 +0100

    included order_by in statement

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,7 +2,7 @@
 
 -- Write a query using an INNER JOIN
 -- to retrieve all bookings and the respective users who made those bookings.
-SELECT * FROM User INNER JOIN Booking ON User.user_id =  Booking.user_id;
+SELECT first_name, last_name, role, b.booking_id, b.property_id, b.user_id, b.start_date, b.end_date, b.status, b.total_price FROM users AS u INNER JOIN booking AS b ON u.user_id =  b.user_id ORDER BY  u.first_name;
 
 -- Left join
 SELECT * FROM Booking LEFT JOIN User ON User.user_id =  Booking.user_id;
