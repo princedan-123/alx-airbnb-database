607bc35ed10bad0153ab69d60497b560df268c88
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-02 05:16:55 +0100

    query whose performance is to be evaluted

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -6,6 +6,6 @@
 --along with the user details, property details, and payment details
 SELECT *
 FROM
-User AS u  INNER JOIN Booking AS b ON b.user_id = u.user_id
-INNER JOIN Property AS p ON b.property_id = p.property_id
-INNER JOIN Payment AS pa ON pa.booking_id = b.booking_id;
+users u  INNER JOIN booking AS b ON b.user_id = u.user_id
+INNER JOIN property AS p ON b.property_id = p.property_id
+INNER JOIN payment AS pa ON pa.booking_id = b.booking_id;
