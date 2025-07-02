e9871efa9e23127609bffd5e9bc7dd8bd65d9d6e
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-07-02 05:27:19 +0100

    used explain for evaluation

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -4,8 +4,9 @@
 
 -- Write an initial query that retrieves all bookings
 --along with the user details, property details, and payment details
-SELECT *
+EXPLAIN SELECT *
 FROM
 users u  INNER JOIN booking AS b ON b.user_id = u.user_id
 INNER JOIN property AS p ON b.property_id = p.property_id
-INNER JOIN payment AS pa ON pa.booking_id = b.booking_id;
+INNER JOIN payment AS pa ON pa.booking_id = b.booking_id
+WHERE b.status = 'confirmed' AND total_price > 0;
